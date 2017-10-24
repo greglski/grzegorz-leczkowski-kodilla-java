3fd7fa77f19e269ec15b20af5717b485ded27a04
Grzegorz Leczkowski <grzegorz.leczkowski@gmail.com>
2017-10-24 23:24:50 +0200

    10.4 Zadanie: Wszystko w jednym

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Arrays;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
