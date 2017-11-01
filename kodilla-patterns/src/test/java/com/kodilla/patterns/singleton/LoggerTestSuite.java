9a8b2625f24e23a6dde0400e8bab2e9e53826864
Grzegorz Leczkowski <grzegorz.leczkowski@gmail.com>
2017-11-01 10:54:29 +0100

    11.1 Wzorce projektowe Singleton v2

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,34 +1,9 @@
 package com.kodilla.patterns.singleton;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 public class LoggerTestSuite {
-
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outputStream));
-    }
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
-    @Test
-    public void testLog() {
-        //Given
-        //When
-        //Logger logger = new Logger();
-        Logger.getInstance().log("Operation unfinished");
-        //Then
-        Assert.assertEquals("Log: [Operation unfinished]\r\n", outputStream.toString());
-    }
-
     @Test
     public void testGetLastLog() {
         //Given
