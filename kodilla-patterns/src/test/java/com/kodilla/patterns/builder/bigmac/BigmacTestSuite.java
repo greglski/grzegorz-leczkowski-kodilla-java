a0b0d3aca6ed9b147c25615fab44ac947dbffedb
Grzegorz Leczkowski <grzegorz.leczkowski@gmail.com>
2017-11-05 20:23:04 +0100

    11.5 Wzorce projektowe Builder v1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Onion")
+                .burgers(2)
+                .sauce("Standard")
+                .ingredient("Chilli")
+                .roll("Sezame")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(2, howManyIngredients);
+    }
+}
